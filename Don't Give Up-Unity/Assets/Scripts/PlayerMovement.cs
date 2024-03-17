423dd8b38734be1ab0d73fae56d375728cb22995
BlinkLudus <conor.sheppard@hotmail.com>
2024-03-17 14:30:51 +0000

    Added animations to the player

--- Don't Give Up-Unity/Assets/Scripts/PlayerMovement.cs
+++ Don't Give Up-Unity/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public Animator animator;
     private float horizontal;
+    private bool isJumping = false;
+    private bool isFalling = false;
     [SerializeField] private float speed = 8f;
     [SerializeField] private float jumpingPower = 16f;
     [SerializeField] private bool isFacingRight = true;
+    [SerializeField] private float fallThreshold = -1f;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -21,15 +24,50 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
+        if (IsGrounded())
+        {
+            if(rb.velocity.y <= 0)
+            {
+                animator.SetBool("IsJumping", false);
+                isJumping = false;
+            }
+            
+        }
+        else
+        {
+            if(rb.velocity.y < fallThreshold)
+            {
+                animator.SetBool("IsFalling", true);
+                isFalling = true;
+                animator.SetBool("IsJumping", false); // Player is falling, not jumping
+                isJumping = false;
+
+            }
+            else
+            {
+                animator.SetBool("IsFalling", false);
+                isFalling = false;
+            }
+
+            animator.SetBool("IsJumping", true);
+            isJumping = true;
+
+        }
+
         if(Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            animator.SetBool("IsJumping", true);
+            isJumping = true;
+
         }
 
         if (Input.GetButtonDown("Jump") && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+           
         }
+
 
         Flip();
     }
@@ -42,6 +80,7 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+     
     }
 
     private void Flip()
